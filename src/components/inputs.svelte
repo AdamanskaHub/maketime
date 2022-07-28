447cae4e41eda93b37b6cb64be6697602c9bd05e
Emilie <adamanska@gmail.com>
2022-07-28 19:31:22 +0200

    adding the values

--- src/components/inputs.svelte
+++ src/components/inputs.svelte
@@ -1,1 +1,24 @@
-<p>inputs</p>
+<script>
+	export let priorities = ['somehting','else'];
+  let info =""
+  function validate() {
+    if (info != '') {
+      console.log(info)
+      priorities = [...priorities, info];
+      info=""
+    }
+    
+	}
+</script>
+
+{#each priorities as priority}
+  <p>{priority}</p>
+{/each}
+
+
+<form on:submit|preventDefault={validate} >
+	<input bind:value={info} type="text">
+	<button type="submit">
+		Add
+	</button>
+</form>
